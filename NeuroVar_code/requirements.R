b7cb9119a9d699d46ae956973494d15ea75e51a6
Hiba Ben Aribi <73958439+HibaBenAribi@users.noreply.github.com>
2023-03-07 11:25:21 +0100

    Update requirements.R

--- NeuroVar_code/requirements.R
+++ NeuroVar_code/requirements.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/LENOVO/Desktop/NeuroVar/version2")
+
 library(shinythemes)
 library(shinyWidgets)
 library(shiny)
@@ -18,9 +18,9 @@ library(ggplot2)
 library(readxl)
 library(readr)
 library(sqldf)
+library(bedr)
 ############
 annotation1 <- read_csv("source_data/annotation1.txt")
-annotation2 <- read_csv("source_data/annotation2.txt")
 full_list <- read_excel("source_data/full_list.xlsx")
 expression <- read_csv("demo_data/expressionALS.csv")
 file_paths_control <- fs::dir_ls("demo_data/snp/control")
