f7dfc5410639dc93cae772611f7d7485c2443617
Hiba Ben Aribi <73958439+HibaBenAribi@users.noreply.github.com>
2024-06-10 15:38:55 +0100

    Update Run_App.R

--- NeuroVar_Shiny_Application/NeurVar_Shiny/Run_App.R
+++ NeuroVar_Shiny_Application/NeurVar_Shiny/Run_App.R
@@ -1,2 +1,9 @@
-source("code.R")
+#  NeuroVar
+## Run the following  code in order
+source("Dependencies_NeuroVar.R")
+source("UI.R",local = TRUE)
 
+source("Server.R",local = TRUE)
+
+# Run the application
+shinyApp(ui = UI, server = Server)
